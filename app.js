01ffe2ecaab475ea89b6b2df990f29ad48ef0743
mjw-emc <60986857+mjw-emc@users.noreply.github.com>
2020-02-13 08:51:37 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+// Set constraints
+var constraints = { video: { facingMode: "user" }, audio: false };
+
+// Define constants
+const cameraView = document.querySelector("#camera--view"),
+      cameraOutput = document.querySelector("#camera--output"),
+      cameraSensor = document.querySelector("#camera--sensor"),
+      cameraTrigger = document.querySelector("#camera--trigger")
+
+// Access the camera and stream 
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(stream) {
+        track = stream.getTracks()[0];
+        cameraView.srcObject = stream;
+    })
+    .catch(function(error) {
+        console.error("Oops. Something is broken.", error);
+    });
+}
+
+// Take a picture when clicked
+cameraTrigger.onclick = function() {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.classList.add("taken");
+};
+
+// Start the video stream when the window loads
+window.addEventListener("load", cameraStart, false);
+
